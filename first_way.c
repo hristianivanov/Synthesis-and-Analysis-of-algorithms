869a44f19538ba5c044d88d2f2001b2565584039
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2025-10-02 02:11:41 +0300

    push first task

---
+++ first_way.c
@@ -0,0 +1,24 @@
+﻿//Task: Find Greatest common divisor(gcd)
+
+/*Реализирайте всеки от алгоритмите за намиране на най - голям общ делител с
+отделна функция, извикайте ги в main функцията и разпечатайте получените
+резултати.*/
+
+#include <stdio.h>
+
+int gcd(int a, int b) {
+	if (a == 0)
+		return b;
+	while (b != 0)
+	{
+		if (a > b)
+			a -= b;
+		else
+			b -= a;
+	}
+	return a;
+}
+
+int main() {
+	printf("%d", gcd(24, 32));
+}
